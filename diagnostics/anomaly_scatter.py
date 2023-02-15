3ca9d21acba27778d73365ed7a9bb068555a7876
thomas.crocker@metoffice.gov.uk <thomas.crocker@metoffice.gov.uk>
2023-02-15 14:45:05 +0000

    swap axes

--- diagnostics/anomaly_scatter.py
+++ diagnostics/anomaly_scatter.py
@@ -20,37 +20,39 @@ logger = logging.getLogger(os.path.basename(__file__))
 
 def _plot_scatter(cfg, data_df):
     logger.info('Making the plot')
+    # get path to output plot folder
     local_path = cfg['plot_dir']
 
-    # do some plotting
+    # Create figure and plot
     fig, ax1 = plt.subplots(1, 1, layout="constrained", figsize=(10, 6))
+    sns.scatterplot(data=data_df, x='tas', y='pr', ax=ax1)
 
-    sns.scatterplot(data=data_df, x='pr', y='tas', ax=ax1)
-
+    # Create labels and annotate scatter points
     labels = []
     i = 1
     for k, v in data_df.iterrows():
         # labels
-        l = f'{i} - {k}'
-        labels.append(l)
+        label = f'{i} - {k}'
+        labels.append(label)
         ax1.annotate(
             i,
-            (v['pr'], v['tas']),
+            (v['tas'], v['pr']),
             xytext=(2.5, -2.5), textcoords='offset points',
             fontsize=8
         )
         i = i+1
 
+    # add labels to graph
     ax1.text(
         1.05, 1.0, '\n'.join(labels),
         transform=ax1.transAxes,
         ha='left',
         va='top'
     )
-
     ax1.set_title('tas and pr anomaly')
-    png_name = 'scatter_plot.png'
 
+    # save and finish
+    png_name = 'scatter_plot.png'
     fig.savefig(os.path.join(local_path, png_name))
     plt.close(fig)
 
@@ -64,37 +66,42 @@ def process_data(cfg):
         string; runs the user diagnostic
 
     """
-    # we want to read the data from the cfg files, and put values into a 
+    # we want to read the data from the cfg files, and put values into a
     # dataframe, arranged by model name / variable
 
-    # first group the data by dataset
+    # first group the data by dataset / model name
     my_files_dict = group_metadata(cfg['input_data'].values(), 'dataset')
 
-    data = {}
-
     logger.info('Processing data')
+    data = {}
+    # for each model...
     for key, value in my_files_dict.items():
-        # process the data
         data[key] = {}
-
+        # for each variable (tas, pr)
         for var in value:
             # load the data
             cube = iris.load_cube(var['filename'])
-            
-            # extract period of interest and mean
-            cube = cube.extract(iris.Constraint(season_year=lambda y: cfg['period_start'] <= y <= cfg['period_end']))
+
+            # extract period of interest and calculate mean
+            cube = cube.extract(
+                iris.Constraint(
+                    season_year=lambda y: cfg['period_start'] <= y <= cfg['period_end']
+                    )
+                )
             cube = cube.collapsed('time', iris.analysis.MEAN)
-            
-            # store the data
+
+            # convert units if needed
             if var['short_name'] == 'pr':
                 # convert precip to mm/day
                 d_val = cube.data.item() * 86400
             else:
                 d_val = cube.data.item()
+            # store the data
             data[key][var['short_name']] = d_val
-            
+
+    # convert to dataframe
     data_df = pd.DataFrame(data).transpose()
-    
+
     # now make the plot
     _plot_scatter(cfg, data_df)
 
